1c5887393cc01c0af7b892a086f95472c1b32d6e
Deb Jagadeba <deb.jagadev@live.com>
2024-02-29 23:26:13 +1100

    utils

---
+++ utils/index.ts
@@ -0,0 +1,30 @@
+import { fakerEN_AU as faker } from '@faker-js/faker'
+import { off } from 'process'
+
+export const wait = async (time: number) =>
+  new Promise((resolve) => setTimeout(resolve, time))
+
+export type User = {
+  firstName: string
+  lastName: string
+  address: string
+  email: string
+  phone: string
+}
+export const user: User = {
+  firstName: faker.person.firstName(),
+  lastName: faker.person.lastName(),
+  address: faker.location.streetAddress({ useFullAddress: true }),
+  email: faker.internet.email(),
+  phone: faker.phone.number(),
+}
+
+export function getAFutureDate(offset: number): string {
+  var currentDate = new Date(
+    new Date().getTime() + offset * 24 * 60 * 60 * 1000
+  )
+  var day = currentDate.getDate()
+  var month = currentDate.getMonth() + 1
+  var year = currentDate.getFullYear()
+  return `${day}-${month}-${year}`
+}
